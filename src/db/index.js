898cbeded6177accec5dd18b9e1f4340c9a5f43a
easethegithub <sutarvaibhavv77@gmail.com>
2025-07-10 16:21:48 +0530

    utils code discussion

--- src/db/index.js
+++ src/db/index.js
@@ -1,17 +1,17 @@
-import mongoose from "mongoose"
-import { Db_name }  from "../constants.js"
-import express from "express"
+// import mongoose from "mongoose"
+// import { Db_name }  from "../constants.js"
+// import express from "express"
 
-const app = express()
-const connectdb = async () => {
-        try{
-            const connectiontoDb = await mongoose.connect(`${process.env.MONGODB_URI}/${Db_name}`)
-            console.log(`mongo db connected DB hosted: ${connectiontoDb.connection.host}`)
-        }
-        catch(error){
-            console.log("connection failed the reasons are as follows ===>",error)
-            process.exit(1)
-        }
-}
+// const app = express()
+// const connectdb = async () => {
+//         try{
+//             const connectiontoDb = await mongoose.connect(`${process.env.MONGODB_URI}/${Db_name}`)
+//             console.log(`mongo db connected DB hosted: ${connectiontoDb.connection.host}`)
+//         }
+//         catch(error){
+//             console.log("connection failed the reasons are as follows ===>",error)
+//             process.exit(1)
+//         }
+// }
 
-export default  connectdb
+// export default  connectdb
